a726099735ee198da3d552bb4ab5b0102338a730
Dylan Beattie <dylan@dylanbeattie.net>
2023-10-10 14:17:57 +0100

    Add Artist entity and DbContext using Sqlite

---
+++ Rockaway/Rockaway.WebApp/Data/RockawayDbContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Rockaway.WebApp.Data.Entities;
+
+namespace Rockaway.WebApp.Data;
+
+public class RockawayDbContext : DbContext {
+	// We must declare a constructor that takes a DbContextOptions<RockawayDbContext>
+	// if we want to use Asp.NET to configure our database connection and provider.
+	public RockawayDbContext(DbContextOptions<RockawayDbContext> options) : base(options) { }
+
+	public DbSet<Artist> Artists { get; set; } = default!;
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder) {
+		base.OnModelCreating(modelBuilder);
+		// Override EF Core's default table naming (which pluralizes entity names)
+		// and use the same names as the C# classes instead
+		var rockawayEntities = modelBuilder.Model.GetEntityTypes().Where(e => e.ClrType.Namespace == typeof(Artist).Namespace);
+		foreach (var entity in rockawayEntities) entity.SetTableName(entity.DisplayName());
+	}
+}
--- Rockaway/Rockaway.WebApp/Program.cs
+++ Rockaway/Rockaway.WebApp/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Rockaway.WebApp.Data;
 using Rockaway.WebApp.Services;
 using Serilog;
 
@@ -6,12 +9,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog();
 
 builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<IStatusReporter>(new StatusReporter());
+
+var sqliteConnection = new SqliteConnection("Data Source=:memory:");
+sqliteConnection.Open();
+builder.Services.AddDbContext<RockawayDbContext>(options => options.UseSqlite(sqliteConnection));
+
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment()) {
 	app.UseExceptionHandler("/Error");
 	app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope()) {
+	using (var db = scope.ServiceProvider.GetService<RockawayDbContext>()!) {
+		db.Database.EnsureCreated();
+	}
 }
 
 app.UseHttpsRedirection();
@@ -23,4 +38,5 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 app.MapGet("/status", (IStatusReporter reporter) => reporter.GetStatus());
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
 app.Run();
