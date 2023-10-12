19445a4957e5778a263820fa611728b53da0adbe
Dylan Beattie <dylan@dylanbeattie.net>
2023-10-12 13:18:44 +0100

    Create DB migration for ASPNET Identity provider.

--- Rockaway/Rockaway.WebApp/Data/RockawayDbContext.cs
+++ Rockaway/Rockaway.WebApp/Data/RockawayDbContext.cs
@@ -18,7 +18,9 @@ public class RockawayDbContext : IdentityDbContext<IdentityUser> {
 		base.OnModelCreating(modelBuilder);
 		// Override EF Core's default table naming (which pluralizes entity names)
 		// and use the same names as the C# classes instead
-		foreach (var entity in modelBuilder.Model.GetEntityTypes()) {
+		var rockawayEntityNamespace = typeof(Artist).Namespace;
+		var rockawayEntities = modelBuilder.Model.GetEntityTypes().Where(e => e.ClrType.Namespace == rockawayEntityNamespace);
+		foreach (var entity in rockawayEntities) {
 			entity.SetTableName(entity.DisplayName());
 		}
 
@@ -32,6 +34,7 @@ public class RockawayDbContext : IdentityDbContext<IdentityUser> {
 
 		modelBuilder.Entity<Artist>().HasData(SampleData.Artists.AllArtists);
 		modelBuilder.Entity<Venue>().HasData(SampleData.Venues.AllVenues);
+		modelBuilder.Entity<IdentityUser>().HasData(SampleData.Users.Admin);
 
 	}
 }
